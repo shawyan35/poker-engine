6a53211236ee3e5e623c83599025e8daf7cfe88b
Shawyan Razavi <shawyan04@yahoo.com>
2026-08-12 21:20:10 -0700

    feat: complete hand evaluator with all hand types and tests

--- src/poker/evaluator.py
+++ src/poker/evaluator.py
@@ -15,7 +15,41 @@ class HandRank(IntEnum):
 
 def evaluate(cards: list[Card]) -> tuple[HandRank, list[int]]:
     """Evaluates hand strength"""
-    pass
+    result = _check_straight_flush(cards)
+    if result:
+        return result
+    
+    result = _check_four_of_a_kind(cards)
+    if result:
+        return result
+    
+    result = _check_full_house(cards)
+    if result:
+        return result
+    
+    result = _check_flush(cards)
+    if result:
+        return result
+    
+    result = _check_straight(cards)
+    if result:
+        return result
+    
+    result = _check_three_of_a_kind(cards)
+    if result:
+        return result
+    
+    result = _check_two_pair(cards)
+    if result:
+        return result
+    
+    result = _check_one_pair(cards)
+    if result:
+        return result
+    
+    result = _check_high_card(cards)
+    return result
+
 
 def _check_flush(cards: list[Card]) -> tuple[HandRank, list[int]] | None:
     suit_counts = Counter(card.suit for card in cards)
@@ -45,3 +79,92 @@ def _check_straight(cards: list[Card]) -> tuple[HandRank, list[int]] | None:
         return (HandRank.STRAIGHT, [5, 4, 3, 2, 1])
     
     return None
+
+def _check_four_of_a_kind(cards: list[Card]) -> tuple[HandRank, list[int]] | None:
+
+    rank_counts = Counter(card.rank for card in cards)
+    frequencies = sorted(rank_counts.values(), reverse=True)
+
+    if frequencies[0] == 4:
+        quads_rank = [rank for rank, count in rank_counts.items() if count == 4][0]
+        kickers = sorted([rank for rank, count in rank_counts.items() if count != 4], reverse=True)
+
+        return(HandRank.FOUR_OF_A_KIND, [quads_rank, kickers[0]])
+    
+    return None
+
+def _check_full_house(cards: list[Card]) -> tuple[HandRank, list[int]] | None:
+
+    rank_counts = Counter(card.rank for card in cards)
+    frequencies = sorted(rank_counts.values(), reverse=True)
+
+    if frequencies[:2] == [3, 2]:
+        three_of_a_kind_rank = [rank for rank, count in rank_counts.items() if count == 3][0]
+        pair_rank = [rank for rank, count in rank_counts.items() if count == 2][0]
+
+        return(HandRank.FULL_HOUSE, [three_of_a_kind_rank, pair_rank])
+    
+    return None
+
+def _check_three_of_a_kind(cards: list[Card]) -> tuple[HandRank, list[int]] | None:
+
+    rank_counts = Counter(card.rank for card in cards)
+    frequencies = sorted(rank_counts.values(), reverse=True)
+
+    if frequencies[0] == 3 and frequencies[1] != 2:
+        three_of_a_kind_rank = [rank for rank, count in rank_counts.items() if count == 3][0]
+        kickers = sorted([rank for rank, count in rank_counts.items() if count != 3], reverse=True)
+
+        return(HandRank.THREE_OF_A_KIND, [three_of_a_kind_rank, kickers[0], kickers[1]])
+   
+    return None
+
+def _check_two_pair(cards: list[Card]) -> tuple[HandRank, list[int]] | None:
+    rank_counts = Counter(card.rank for card in cards)
+    frequencies = sorted(rank_counts.values(), reverse=True)
+
+    if frequencies[:2] == [2,2]:
+        pair_ranks = sorted([rank for rank, count in rank_counts.items() if count == 2], reverse=True)
+        kickers = sorted([rank for rank, count in rank_counts.items() if count != 2], reverse=True)
+
+        return(HandRank.TWO_PAIR, [pair_ranks[0], pair_ranks[1], kickers[0]])
+    
+    return None
+
+
+def _check_one_pair(cards: list[Card]) -> tuple[HandRank, list[int]] | None:
+    rank_counts = Counter(card.rank for card in cards)
+    frequencies = sorted(rank_counts.values(), reverse=True)
+
+    if frequencies[0] == 2:
+        pair_rank = [rank for rank, count in rank_counts.items() if count == 2][0]
+        kickers = sorted([rank for rank, count in rank_counts.items() if count != 2], reverse=True)
+
+        return(HandRank.ONE_PAIR, [pair_rank] + kickers[:3])
+    
+    return None
+
+def _check_straight_flush(cards: list[Card]) -> tuple[HandRank, list[int]] | None:
+    if _check_flush(cards) is None:
+        return None
+    suit_counts = Counter(card.suit for card in cards)
+
+    for suit, count in suit_counts.items():
+        if count >= 5:
+            flush_cards = [card for card in cards if card.suit == suit]
+
+            straight_result = _check_straight(flush_cards)
+        
+            if straight_result is None:
+                return None
+            else:
+                return (HandRank.STRAIGHT_FLUSH, straight_result[1])
+    
+    return None
+            
+
+def _check_high_card(cards: list[Card]) -> tuple[HandRank, list[int]] | None:
+
+    top_ranks = sorted((card.rank for card in cards), reverse=True)[:5]
+    
+    return (HandRank.HIGH_CARD, top_ranks)
--- tests/test_evaluator.py
+++ tests/test_evaluator.py
@@ -1,22 +1,70 @@
 from poker.card import Card, Rank, Suit
-from poker.evaluator import HandRank, _check_flush, _check_straight
+from poker.evaluator import HandRank, _check_flush, _check_straight, evaluate
 
-card1 = Card(Rank.ACE, Suit.SPADE) # Ace straight
-card2 = Card(Rank.FIVE, Suit.SPADE) # Ace straight
-card3 = Card(Rank.KING, Suit.SPADE) # straight
-card4 = Card(Rank.JACK, Suit.SPADE) # straight
-card5 = Card(Rank.THREE, Suit.SPADE) # Ace straight
-card6 = Card(Rank.FIVE, Suit.DIAMOND) 
-card7 = Card(Rank.ACE, Suit.DIAMOND)
-card8 = Card(Rank.TEN, Suit.CLUB) # straight
-card9 = Card(Rank.TWO, Suit.HEART) # Ace straight
-card10 = Card(Rank.FOUR, Suit.HEART) # Ace straight
-card11 = Card(Rank.NINE, Suit.HEART) # straight
-card12 =Card(Rank.QUEEN, Suit.CLUB) # straight
+# Spades
+card1 = Card(Rank.ACE, Suit.SPADE)
+card2 = Card(Rank.TWO, Suit.SPADE)
+card3 = Card(Rank.THREE, Suit.SPADE)
+card4 = Card(Rank.FOUR, Suit.SPADE)
+card5 = Card(Rank.FIVE, Suit.SPADE)
+card6 = Card(Rank.SIX, Suit.SPADE)
+card7 = Card(Rank.SEVEN, Suit.SPADE)
+card8 = Card(Rank.EIGHT, Suit.SPADE)
+card9 = Card(Rank.NINE, Suit.SPADE)
+card10 = Card(Rank.TEN, Suit.SPADE)
+card11 = Card(Rank.JACK, Suit.SPADE)
+card12 = Card(Rank.QUEEN, Suit.SPADE)
+card13 = Card(Rank.KING, Suit.SPADE)
 
+# Hearts
+card14 = Card(Rank.ACE, Suit.HEART)
+card15 = Card(Rank.TWO, Suit.HEART)
+card16 = Card(Rank.THREE, Suit.HEART)
+card17 = Card(Rank.FOUR, Suit.HEART)
+card18 = Card(Rank.FIVE, Suit.HEART)
+card19 = Card(Rank.SIX, Suit.HEART)
+card20 = Card(Rank.SEVEN, Suit.HEART)
+card21 = Card(Rank.EIGHT, Suit.HEART)
+card22 = Card(Rank.NINE, Suit.HEART)
+card23 = Card(Rank.TEN, Suit.HEART)
+card24 = Card(Rank.JACK, Suit.HEART)
+card25 = Card(Rank.QUEEN, Suit.HEART)
+card26 = Card(Rank.KING, Suit.HEART)
+
+# Diamonds
+card27 = Card(Rank.ACE, Suit.DIAMOND)
+card28 = Card(Rank.TWO, Suit.DIAMOND)
+card29 = Card(Rank.THREE, Suit.DIAMOND)
+card30 = Card(Rank.FOUR, Suit.DIAMOND)
+card31 = Card(Rank.FIVE, Suit.DIAMOND)
+card32 = Card(Rank.SIX, Suit.DIAMOND)
+card33 = Card(Rank.SEVEN, Suit.DIAMOND)
+card34 = Card(Rank.EIGHT, Suit.DIAMOND)
+card35 = Card(Rank.NINE, Suit.DIAMOND)
+card36 = Card(Rank.TEN, Suit.DIAMOND)
+card37 = Card(Rank.JACK, Suit.DIAMOND)
+card38 = Card(Rank.QUEEN, Suit.DIAMOND)
+card39 = Card(Rank.KING, Suit.DIAMOND)
+
+# Clubs
+card40 = Card(Rank.ACE, Suit.CLUB)
+card41 = Card(Rank.TWO, Suit.CLUB)
+card42 = Card(Rank.THREE, Suit.CLUB)
+card43 = Card(Rank.FOUR, Suit.CLUB)
+card44 = Card(Rank.FIVE, Suit.CLUB)
+card45 = Card(Rank.SIX, Suit.CLUB)
+card46 = Card(Rank.SEVEN, Suit.CLUB)
+card47 = Card(Rank.EIGHT, Suit.CLUB)
+card48 = Card(Rank.NINE, Suit.CLUB)
+card49 = Card(Rank.TEN, Suit.CLUB)
+card50 = Card(Rank.JACK, Suit.CLUB)
+card51 = Card(Rank.QUEEN, Suit.CLUB)
+card52 = Card(Rank.KING, Suit.CLUB)
+
+## FLUSH TESTS
 def test_flush_detection():
 
-    flush_cards = [card1, card2, card3, card4, card5, card6, card7]
+    flush_cards = [card1, card13, card11, card5, card3, card47, card17]
     result = _check_flush(flush_cards)
 
     assert result[0] == HandRank.FLUSH
@@ -24,26 +72,172 @@ def test_flush_detection():
 
 def test_no_flush():
 
-    no_flush_cards = [card8, card2, card3, card4, card5, card6, card7]
+    no_flush_cards = [card18, card24, card39, card4, card52, card6, card23]
 
-    result2 = _check_flush(no_flush_cards)  
+    result = _check_flush(no_flush_cards)  
 
-    assert result2 is None
+    assert result is None
 
+
+## STRAIGHT TESTS
 def test_ace_straight():
-    ace_straight_cards = [card1, card2, card5, card9, card10]
-    result3 = _check_straight(ace_straight_cards)
-    assert result3[0] == HandRank.STRAIGHT
-    assert result3[1] == [5, 4, 3, 2, 1]
+    ace_straight_cards = [card14, card2, card3, card17, card44]
+    result = _check_straight(ace_straight_cards)
+    assert result[0] == HandRank.STRAIGHT
+    assert result[1] == [5, 4, 3, 2, 1]
 
 def test_straight():
-    straight_cards = [card3, card4, card8, card11, card12]
-    result4 = _check_straight(straight_cards)
-    assert result4[0] == HandRank.STRAIGHT
-    assert result4[1] == [13, 12, 11, 10, 9]
+    straight_cards = [card13, card38, card11, card23, card9]
+    result = _check_straight(straight_cards)
+    assert result[0] == HandRank.STRAIGHT
+    assert result[1] == [13, 12, 11, 10, 9]
+
 def test_no_straight():
-    no_straight_cards = [card1, card2, card3, card4, card6]
-    result5 = _check_straight(no_straight_cards)
-    assert result5 is None
+    no_straight_cards = [card1, card14, card33, card42, card16]
+    result = _check_straight(no_straight_cards)
+    assert result is None
 
+## QUADS TESTS
 
+def test_quads():
+    quads_cards = [card1, card14, card27, card40, card2, card45, card18]
+    result = evaluate(quads_cards)
+
+    assert result[0] == HandRank.FOUR_OF_A_KIND
+    assert result[1] == [14, 6]
+
+def test_quads2():
+    quads_cards = [card3, card16, card29, card42, card5, card47, card20]
+    result = evaluate(quads_cards)
+
+    assert result[0] == HandRank.FOUR_OF_A_KIND
+    assert result[1] == [3, 8]
+
+def test_no_quads():
+    quads_cards = [card4, card13, card29, card42, card5, card47, card20]
+    result = evaluate(quads_cards)
+
+    assert result != HandRank.FOUR_OF_A_KIND
+
+## FULL HOUSE TESTS
+
+def test_full_house():
+    boat_cards = [card1, card14, card2, card15, card28, card47, card20]
+    result = evaluate(boat_cards)
+
+    assert result[0] == HandRank.FULL_HOUSE
+    assert result[1] == [2, 14]
+
+def test_full_house2():
+    boat_cards = [card4, card17, card5, card18, card31, card50, card23]
+    result = evaluate(boat_cards)
+
+    assert result[0] == HandRank.FULL_HOUSE
+    assert result[1] == [5, 4]
+
+def test_no_full_house():
+    no_boat_cards = [card6, card11, card7, card34, card51, card16, card31]
+    result = evaluate(no_boat_cards)
+
+    assert result != HandRank.FULL_HOUSE
+
+## STRAIGHT FLUSH TESTS
+
+def test_straight_flush():
+    wheel_straight_flush_cards = [card1, card2, card3, card4, card5, card31, card43]
+    result = evaluate(wheel_straight_flush_cards)
+
+    assert result[0] == HandRank.STRAIGHT_FLUSH
+    assert result[1] == [5, 4, 3, 2, 1]
+
+def test_straight_flush2():
+    straight_flush_cards = [card6, card7, card8, card9, card10, card31, card43]
+    result = evaluate(straight_flush_cards)
+
+    assert result[0] == HandRank.STRAIGHT_FLUSH
+    assert result[1] == [10, 9, 8, 7, 6]
+
+def test_no_straight_flush():
+    no_straight_flush_cards = [card8, card2, card13, card4, card5, card31, card43]
+    result = evaluate(no_straight_flush_cards)
+
+    assert result != HandRank.STRAIGHT_FLUSH
+
+## TRIPS TESTS
+
+def test_trips():
+    trips_cards = [card8, card21, card34, card1, card2, card43, card44]
+    result = evaluate(trips_cards)
+
+    assert result[0] == HandRank.THREE_OF_A_KIND
+    assert result[1] == [8, 14, 5]
+
+def test_trips2():
+    trips_cards = [card7, card20, card29, card33, card2, card43, card44]
+    result = evaluate(trips_cards)
+
+    assert result[0] == HandRank.THREE_OF_A_KIND
+    assert result[1] == [7, 5, 4]
+
+def test_no_trips():
+    no_trip_cards = [card6, card20, card29, card33, card2, card43, card44]
+    result = evaluate(no_trip_cards)
+
+    assert result != HandRank.THREE_OF_A_KIND
+
+## TWO PAIR TESTS
+
+def test_two_pair():
+    two_pair_cards = [card1, card14, card2, card15, card52, card51, card50]
+    result = evaluate(two_pair_cards)
+
+    assert result[0] == HandRank.TWO_PAIR
+    assert result[1] == [14, 2, 13]
+
+def test_two_pair2():
+    two_pair_cards = [card3, card16, card6, card19, card52, card51, card50]
+    result = evaluate(two_pair_cards)
+
+    assert result[0] == HandRank.TWO_PAIR
+    assert result[1] == [6, 3, 13]
+
+def test_no_two_pair():
+    two_pair_cards = [card16, card14, card2, card15, card52, card51, card50]
+    result = evaluate(two_pair_cards)
+
+    assert result != HandRank.TWO_PAIR
+
+## ONE PAIR TESTS
+def test_one_pair():
+    one_pair_cards = [card13, card26, card27, card50, card5, card9, card34]
+    result = evaluate(one_pair_cards)
+
+    assert result[0] == HandRank.ONE_PAIR
+    assert result[1] == [13, 14, 11, 9]
+
+def test_one_pair2():
+    one_pair_cards = [card15, card28, card27, card50, card5, card9, card34]
+    result = evaluate(one_pair_cards)
+
+    assert result[0] == HandRank.ONE_PAIR
+    assert result[1] == [2, 14, 11, 9]
+
+def test_no_one_pair():
+    one_pair_cards = [card13, card29, card27, card50, card5, card9, card34]
+    result = evaluate(one_pair_cards)
+
+    assert result != HandRank.ONE_PAIR
+
+## HIGH CARD TESTS
+def test_high_card():
+    high_card_cards = [card14, card30, card28, card51, card6, card10, card35]
+    result = evaluate(high_card_cards)
+
+    assert result[0] == HandRank.HIGH_CARD
+    assert result[1] == [14, 12, 10, 9, 6]
+
+def test_no_high_card():
+    high_card_cards = [card19, card30, card28, card51, card6, card10, card35]
+    result = evaluate(high_card_cards)
+
+    assert result != HandRank.HIGH_CARD
